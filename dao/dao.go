850b6d977d2a71a07de6ee9fd2f7354aa81ef5a4
Claude-Zq <1574089391@qq.com>
2022-04-30 17:25:20 +0800

    启动服务器时，立即检查数据库连接情况

--- dao/dao.go
+++ dao/dao.go
@@ -1,13 +1,24 @@
 package dao
 
-import "database/sql"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
 
 var dB *sql.DB
 
+//连接上数据库
 func InitDB() {
-	db, err := sql.Open("mysql", "root:Zy08294641.@tcp(127.0.0.1:3306)/message_board?charset=utf8mb4&parseTime=True&loc=Local")
+
+	db, err := sql.Open("mysql", "root:Zhouqing123456//@tcp(127.0.0.1:3306)/message_board?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
+	}
+
+	//检查数据库是否可用可访问
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
 	}
 	dB = db
 }

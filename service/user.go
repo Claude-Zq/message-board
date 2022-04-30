da5f447e9a8a2fe92c995a2e7d75f78598ab734a
Claude-Zq <1574089391@qq.com>
2022-04-30 13:29:48 +0800

    添加了密码验证的接口 将用户输入的密码与数据库记录的密码对比，相同返回true不同返回false

--- service/user.go
+++ service/user.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"database/sql"
+	"message-board/dao"
+)
+
+func ChangePassword(username, newPassword string) error {
+
+	return nil
+
+}
+
+//判断密码是否与数据库中的密码相符
+func IsPasswordCorrect(username, password string) (bool, error) {
+	user, err := dao.SelectUserByUsername(username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+	if user.Password != password {
+		return false, nil
+	}
+	return true, nil
+}

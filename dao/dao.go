f26d7c43ba5d854933df16a00afdf04d442eaa03
Claude-Zq <1574089391@qq.com>
2022-04-30 13:17:19 +0800

    连接上本地的数据库message_board

--- dao/dao.go
+++ dao/dao.go
@@ -1,1 +1,13 @@
 package dao
+
+import "database/sql"
+
+var dB *sql.DB
+
+func InitDB() {
+	db, err := sql.Open("mysql", "root:Zy08294641.@tcp(127.0.0.1:3306)/message_board?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	dB = db
+}

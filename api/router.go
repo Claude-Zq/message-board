2985ce06fa5ddd722cba7d0d8b9bcac6230b60b5
Claude-Zq <1574089391@qq.com>
2022-04-30 21:46:26 +0800

    添加了获取密保问题的接口

--- api/router.go
+++ api/router.go
@@ -7,13 +7,15 @@ import (
 func InitEngine() {
 	engine := gin.Default()
 
-	engine.POST("/register", register) //注册
-	engine.POST("/login", login)       //登陆
+	engine.POST("/register", register)   //注册
+	engine.POST("/login", login)         //登陆
+	engine.GET("/question", getQuestion) //获取密保问题
 
 	userGroup := engine.Group("/user")
 	{
 		userGroup.Use(auth)
 		userGroup.POST("/password", changePassword) //修改密码
+
 	}
 	postGroup := engine.Group("/post")
 	{

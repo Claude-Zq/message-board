cf89c795e0d826ca6185b5a19b58a85a03b0651d
Claude-Zq <1574089391@qq.com>
2022-04-30 14:57:56 +0800

    完成修改密码的功能

--- api/user.go
+++ api/user.go
@@ -1,16 +1,55 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"message-board/service"
+	"message-board/tool"
 	"net/http"
 )
 
 func changePassword(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "修改密码")
+	oldPassword := ctx.PostForm("old_password")
+	newPassword := ctx.PostForm("new_password")
+
+	iUsername, _ := ctx.Get("username")
+	username := iUsername.(string)
+
+	//检验旧密码是否正确
+	flag, err := service.IsPasswordCorrect(username, oldPassword)
+	if err != nil {
+		fmt.Println("judge password correct err:", err)
+		return
+	}
+	if !flag {
+		tool.RespErrorWithData(ctx, "旧密码错误")
+		return
+	}
+	//修改新密码
+	err = service.ChangePassword(username, newPassword)
+	if err != nil {
+		fmt.Println("change password err:", err)
+		return
+	}
+	tool.RespSuccessful(ctx)
 }
 
 func login(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "用户登陆")
+	username := ctx.PostForm("username")
+	password := ctx.PostForm("password")
+
+	flag, err := service.IsPasswordCorrect(username, password)
+	if err != nil {
+		fmt.Println("judge password correct err:", err)
+		tool.RespInternalError(ctx)
+		return
+	}
+	if !flag {
+		tool.RespErrorWithData(ctx, "密码错误")
+		return
+	}
+	ctx.SetCookie("username", username, 700, "/", "", false, false)
+	tool.RespSuccessful(ctx)
 }
 
 func register(ctx *gin.Context) {

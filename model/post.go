8d51b5e49f0cbeb227210cf96921d2a7c9d4aaef
Claude-Zq <1574089391@qq.com>
2022-05-01 09:51:19 +0800

    将post的Posttime字段修改为time.Time类型

--- model/post.go
+++ model/post.go
@@ -1,10 +1,12 @@
 package model
 
+import "time"
+
 type Post struct {
-	Id         int    `json:"id"`
-	CommentNum int    `json:"comment_num"`
-	Txt        string `json:"txt"`
-	Username   string `json:"username"`
-	PostTime   string `json:"post_time"`
-	UpdateTime string `json:"update_time"`
+	Id         int       `json:"id"`
+	CommentNum int       `json:"comment_num"`
+	Txt        string    `json:"txt"`
+	Username   string    `json:"username"`
+	PostTime   time.Time `json:"post_time"`
+	UpdateTime time.Time `json:"update_time"`
 }

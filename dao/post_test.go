46f744af21fb4abc31752d56d27e7083c9c53297
Claude-Zq <1574089391@qq.com>
2022-05-01 10:53:45 +0800

    解决了测试时出现的bug 在测试前需要初始化数据库的连接

--- dao/post_test.go
+++ dao/post_test.go
@@ -1,1 +1,32 @@
-package dao
+package dao_test
+
+import (
+	"fmt"
+	"message-board/dao"
+	"message-board/model"
+	"testing"
+	"time"
+)
+
+func TestSelectPostById(t *testing.T) {
+	dao.InitDB()
+	p := model.Post{}
+	p, _ = dao.SelectPostById(1)
+	t.Log(p)
+}
+
+func TestInsertPost(t *testing.T) {
+	dao.InitDB()
+	p := model.Post{
+		CommentNum: 0,
+		Txt:        "这是张三的留言",
+		Username:   "zhangsan",
+		PostTime:   time.Now(),
+		UpdateTime: time.Now(),
+	}
+	err := dao.InsertPost(p)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}

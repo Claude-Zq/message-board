afaf936987caedca056135bbce281e2cb8903b51
Claude-Zq <1574089391@qq.com>
2022-05-01 15:47:52 +0800

    新增根据留言编号获取评论的接口

--- dao/comment.go
+++ dao/comment.go
@@ -9,3 +9,23 @@ func InsertComment(comment model.Comment) error {
 	}
 	return nil
 }
+
+func SelectCommentByPostId(postId int) ([]model.Comment, error) {
+	var comments []model.Comment
+
+	rows, err := dB.Query("SELECT id,post_id,username,txt,comment_time FROM comment WHERE post_id = ?", postId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	
+	for rows.Next() {
+		var comment model.Comment
+		err = rows.Scan(&comment.Id, &comment.PostId, &comment.Username, &comment.Txt, &comment.CommentTime)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, comment)
+	}
+	return comments, nil
+}

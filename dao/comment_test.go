edf3f66383ccc0498f3d89482e29984c967b53f6
Claude-Zq <1574089391@qq.com>
2022-05-01 15:33:04 +0800

    测试了添加评论的接口

--- dao/comment_test.go
+++ dao/comment_test.go
@@ -1,1 +1,22 @@
-package dao
+package dao_test
+
+import (
+	"message-board/dao"
+	"message-board/model"
+	"testing"
+	"time"
+)
+
+func TestInsertComment(t *testing.T) {
+	dao.InitDB()
+	comment := model.Comment{
+		PostId:      2,
+		Txt:         "这是测试代码插入的评论",
+		Username:    "admin",
+		CommentTime: time.Now(),
+	}
+	err := dao.InsertComment(comment)
+	if err != nil {
+		t.Log(err)
+	}
+}

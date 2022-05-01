16b1de0c71231abfeb83318d7a7674951e8753a4
Claude-Zq <1574089391@qq.com>
2022-05-01 13:01:14 +0800

    修复了返回不了数据的bug

--- tool/resp.go
+++ tool/resp.go
@@ -26,6 +26,6 @@ func RespSuccessful(ctx *gin.Context) {
 func ResSuccessfulWithData(ctx *gin.Context, data interface{}) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"info": "成功",
-		"data": "data",
+		"data": data,
 	})
 }

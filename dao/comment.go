8c57f97989673b0419787583eb2720b3f5c12882
Claude-Zq <1574089391@qq.com>
2022-05-01 15:25:52 +0800

    新增插入评论的接口

--- dao/comment.go
+++ dao/comment.go
@@ -1,1 +1,11 @@
 package dao
+
+import "message-board/model"
+
+func InsertComment(comment model.Comment) error {
+	_, err := dB.Exec("INSERT INTO comment(post_id,username,comment_time,txt) VALUES(?,?,?,?)", comment.PostId, comment.Username, comment.CommentTime, comment.Txt)
+	if err != nil {
+		return err
+	}
+	return nil
+}

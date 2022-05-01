9be008d793bfa389eb24244901fe7d989c1eb97d
Claude-Zq <1574089391@qq.com>
2022-05-01 16:32:39 +0800

    新增添加评论的接口

--- api/comment.go
+++ api/comment.go
@@ -1,11 +1,39 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"message-board/model"
+	"message-board/service"
+	"message-board/tool"
+	"strconv"
+	"time"
 )
 
 func addComment(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "添加评论")
+	iUsername, _ := ctx.Get("username")
+	username := iUsername.(string)
 
+	txt := ctx.PostForm("txt")
+	postIdString := ctx.PostForm("post_id")
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		fmt.Println("postId string to int err", err)
+		tool.RespErrorWithData(ctx, "文章id有误")
+		return
+	}
+
+	comment := model.Comment{
+		PostId:      postId,
+		Txt:         txt,
+		Username:    username,
+		CommentTime: time.Now(),
+	}
+	err = service.AddComment(comment)
+	if err != nil {
+		fmt.Println("add comment err:", err)
+		tool.RespInternalError(ctx)
+		return
+	}
+	tool.RespSuccessful(ctx)
 }

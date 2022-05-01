4653adcd58acc48584bae67b64d6001136058ddf
Claude-Zq <1574089391@qq.com>
2022-05-01 10:56:14 +0800

    新增user_test.go的测试文件

---
+++ dao/user_test.go
@@ -0,0 +1,32 @@
+package dao_test
+
+import (
+	"message-board/dao"
+	"testing"
+)
+
+func TestGetQuestionByUsername(t *testing.T) {
+	dao.InitDB()g
+	question0, err0 := dao.GetQuestionByUsername("zhangsan")
+	t.Log(question0, err0)
+	question1, err1 := dao.GetQuestionByUsername("claude")
+	t.Log(question1, err1)
+	question2, err2 := dao.GetQuestionByUsername("ahhahha")
+	t.Log(question2, err2)
+
+}
+
+func TestSelectUserByUsername(t *testing.T) {
+	dao.InitDB()
+	u, err := dao.SelectUserByUsername("zhangsan")
+	t.Log(u, err)
+}
+
+func TestUpdatePassword(t *testing.T) {
+	dao.InitDB()
+	err := dao.UpdatePassword("zhangsan", "321")
+	if err != nil {
+		t.Log(err)
+		return
+	}
+}

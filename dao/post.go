fc972426beb3163e571d38f32746053a7250e1f0
Claude-Zq <1574089391@qq.com>
2022-05-01 10:52:02 +0800

    新增按Id查找留言的接口

--- dao/post.go
+++ dao/post.go
@@ -6,3 +6,19 @@ func InsertPost(post model.Post) error {
 	_, err := dB.Exec("INSERT INTO post(username,txt,post_time,update_time) values(?,?,?,?);", post.Username, post.Txt, post.PostTime, post.UpdateTime)
 	return err
 }
+
+func SelectPostById(postId int) (model.Post, error) {
+	post := model.Post{}
+
+	row := dB.QueryRow("SELECT id,username,comment_num,txt,post_time,update_time FROM post WHERE id = ?", postId)
+
+	if row.Err() != nil {
+		return model.Post{}, row.Err()
+	}
+
+	err := row.Scan(&post.Id, &post.Username, &post.CommentNum, &post.Txt, &post.PostTime, &post.UpdateTime)
+	if err != nil {
+		return model.Post{}, err
+	}
+	return post, nil
+}

6b951e901e95cfa77ca8eaaa26f07d84b78addac
Claude-Zq <1574089391@qq.com>
2022-05-02 11:14:54 +0800

    修改了发布评论的接口 发表评论后,对应留言的评论数+1

--- service/comment.go
+++ service/comment.go
@@ -6,7 +6,16 @@ import (
 )
 
 func AddComment(comment model.Comment) error {
-	return dao.InsertComment(comment)
+	err := dao.InsertComment(comment)
+	if err != nil {
+		return err
+	}
+	post, err := GetPostById(comment.PostId)
+	if err != nil {
+		return err
+	}
+	err = dao.UpdatePostCommentNum(post.Id, post.CommentNum+1)
+	return err
 }
 
 func GetPostComments(postId int) ([]model.Comment, error) {

83de83119493ed7d6247f96c055b17352be54613
Claude-Zq <1574089391@qq.com>
2022-05-04 20:28:54 +0800

    新增用于登陆和注册的绑定模型

--- model/user.go
+++ model/user.go
@@ -5,3 +5,8 @@ type User struct {
 	Username string
 	Password string
 }
+
+type Login struct {
+	Username string `form:"username" json:"username" xml:"username"  binding:"required,lengthOk"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required,lengthOk"`
+}

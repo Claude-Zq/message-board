3197911de2807c5695b3672bc66e81cd44a80900
Claude-Zq <1574089391@qq.com>
2022-05-04 20:30:45 +0800

    新增用于限制密码和用户名长度的验证器

--- tool/valid.go
+++ tool/valid.go
@@ -1,1 +1,19 @@
 package tool
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+const maxLen = 15
+
+//限制密码和用户名的验证器
+var LengthOk validator.Func = func(fl validator.FieldLevel) bool {
+	data, ok := fl.Field().Interface().(string)
+	if !ok {
+		return false
+	}
+	if len(data) > maxLen {
+		return false
+	}
+	return true
+}

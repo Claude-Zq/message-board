e36f85243631a3f1e435642bdad7ab51739a5de6
Claude-Zq <1574089391@qq.com>
2022-05-04 20:33:24 +0800

    注册了校验用户名和密码长度的验证器

--- api/router.go
+++ api/router.go
@@ -1,11 +1,24 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"message-board/tool"
 )
 
 func InitEngine() {
 	engine := gin.Default()
+
+	//注册验证器
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		err := v.RegisterValidation("lengthOk", tool.LengthOk)
+		if err != nil {
+			fmt.Println("验证器注册失败")
+			return
+		}
+	}
 
 	engine.POST("/register", register)   //注册
 	engine.POST("/login", login)         //登陆
